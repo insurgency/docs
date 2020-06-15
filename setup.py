5560428c999c7280735421c97b7eef781d8054d4
h1nk <12993584+h1nk@users.noreply.github.com>
2020-06-15 18:28:34 -0400

    Update sphinx-autodoc-typehints package to development commit
    This fixes a warning emitted by Sphinx
    See: agronholm/sphinx-autodoc-typehints#133

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     ],
     install_requires=[
         'Sphinx',
-        'sphinx-autodoc-typehints',
+        'git+https://github.com/agronholm/sphinx-autodoc-typehints.git@2dae2d685a5edf82c1ee3d919ff9434c2a1622f8#egg'
+        '=sphinx-autodoc-typehints',
     ],
 )
